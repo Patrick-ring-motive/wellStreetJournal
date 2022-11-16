ed5e189cadf9fb01e2d47bfb0acd1cc4983881e9
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2022-11-16 13:52:50 -0600

    Update wsjGoogleAnalytics.js

--- alt/js/wsjGoogleAnalytics.js
+++ alt/js/wsjGoogleAnalytics.js
@@ -7,11 +7,9 @@
 
   ga('create', 'G-N7VHFMTL4E', 'auto');
   ga('send', 'pageview');
-</script>
-<!-- End Google Analytics -->
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-N7VHFMTL4E"></script>
-<script>
+
+
+
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
