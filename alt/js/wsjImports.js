75437ac62575cf834309a030c9d66396c41bb43d
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2022-11-16 15:28:04 -0600

    Update wsjImports.js

--- alt/js/wsjImports.js
+++ alt/js/wsjImports.js
@@ -2,7 +2,7 @@
 
 setTimeout(async function(){
   
-const URLs=['/alt/js/wsjGoogleAnalytics.js','/alt/js/dwsjGoogleAnalytics.js','/alt/js/wsjFixLinks.js','/alt/js/wsjSeo.js'];
+const URLs=['/alt/js/sw/swRegister.js','/alt/js/wsjGoogleAnalytics.js','/alt/js/dwsjGoogleAnalytics.js','/alt/js/wsjFixLinks.js','/alt/js/wsjSeo.js'];
 const URLs_length=URLs.length;
 
 for(let i=0;i<URLs_length;i++){try{
