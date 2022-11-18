32086a250376d45b22e140e1642187d7a1695939
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2022-11-17 22:49:15 -0600

    Update swRegister.js

--- alt/js/sw/swRegister.js
+++ alt/js/sw/swRegister.js
@@ -1,10 +1,5 @@
 
-if(document.domain.indexOf('dwsj')>-1){
-   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/dsw.js?wellStreetJournalAlt');
-  } 
-}else{
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js?wellStreetJournalAlt');
   }
-}
+
