1ad8404a98498f80bada63e464b0a96b1d6d56e3
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2022-11-29 09:41:17 -0600

    Update wsjFixBrokenTags.js

--- alt/js/wsjFixBrokenTags.js
+++ alt/js/wsjFixBrokenTags.js
@@ -1,1 +1,34 @@
 
+fixScriptTags();
+setInterval(async function(){fixScriptTags();},1002);
+
+
+
+async function fixScriptTags(){
+  
+const scripts = document.getElementsByTagName('script');
+const scripts_length = scripts.length;
+  
+for(let i=0;i!=scripts_length;i++){try{
+let script_content = scripts[i].innerHTML.toString();
+  if(script_content.length > 0){
+  
+  let script_rewrite=replaceEscapes(script_content);
+    if(script_content.length>script_rewrite.length){
+    
+    
+    
+    }
+  }
+}catch(e){continue;}}
+
+
+
+}
+
+
+function replaceEscapes(str){
+
+return str.replaceAll('&amp;','&').replaceAll('&lt;','<').replaceAll('&gt;','>');
+
+}
