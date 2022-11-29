a25552147b2bef333114be1adf8ebcf585dcd329
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2022-11-29 09:50:44 -0600

    Update wsjFixBrokenTags.js

--- alt/js/wsjFixBrokenTags.js
+++ alt/js/wsjFixBrokenTags.js
@@ -16,7 +16,30 @@ let script_content = scripts[i].innerHTML.toString();
   let script_rewrite=replaceEscapes(script_content);
     if(script_content.length>script_rewrite.length){
     
+    recreateScript(scripts[i],script_rewrite);
     
+    }
+  }
+}catch(e){continue;}}
+
+
+
+}
+
+
+async function fixStyleTags(){
+  
+const styles = document.getElementsByTagName('style');
+const styles_length = styles.length;
+  
+for(let i=0;i!=styles_length;i++){try{
+let style_content = styles[i].innerHTML.toString();
+  if(style_content.length > 0){
+  
+  let style_rewrite=replaceEscapes(style_content);
+    if(style_content.length>style_rewrite.length){
+    
+    recreateStyle(styles[i],style_rewrite);
     
     }
   }
@@ -31,4 +54,27 @@ function replaceEscapes(str){
 
 return str.replaceAll('&amp;','&').replaceAll('&lt;','<').replaceAll('&gt;','>');
 
+}
+
+function recreateScript(elem,str){
+  
+  let new_script = elem.cloneNode(false);
+  let script_parent=elem.parentNode();
+
+  new_script.innerHTML = str;
+  script_parent.removeChild(elem);
+  script_parent.appendChild(new_script);
+  
+}
+
+
+function recreateStyle(elem,str){
+  
+  let new_style = elem.cloneNode(false);
+  let style_parent=elem.parentNode();
+
+  new_style.innerHTML = str;
+  style_parent.removeChild(elem);
+  style_parent.appendChild(new_style);
+  
 }
