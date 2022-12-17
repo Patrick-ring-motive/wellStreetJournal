59bab49683b743dd25e42952f3e1a0dc1de6a172
oddjobhat <60206943+Patrick-ring-motive@users.noreply.github.com>
2022-12-17 21:30:32 +0000

    lazy link caching works

--- alt/js/lib/lazyLoadLinks.js
+++ alt/js/lib/lazyLoadLinks.js
@@ -22,13 +22,13 @@ function loadLink(anchor) {
   let l = document.createElement('link');
   l.rel = "prefetch";
   l.href = anchor.href;
- // document.body.appendChild(l);
+  document.body.appendChild(l);
   if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
 
     let s = document.createElement('script');
     s.defer = 'defer';
     s.src = anchor.href;
-    s.setAttribute('lazyLinkLoader','lazyLinkLoader');
+    s.setAttribute('lazyLinkLoader', 'lazyLinkLoader');
     document.body.appendChild(s);
 
   }
@@ -43,7 +43,7 @@ function loadLink(anchor) {
 }
 
 function lazyLoadLinks() {
-  
+
   const article_links = document.querySelectorAll('a[href*="articles"]:not([lazyLoaded])');
   const article_links_length = article_links.length;
   for (let i = 0; i < article_links_length; i++) {
@@ -56,7 +56,7 @@ function lazyLoadLinks() {
         loadLink(la);
         return;
       }
-    } catch (e) {continue;}
+    } catch (e) { continue; }
   }
 
   const links = document.querySelectorAll('a:not([lazyLoaded])');
@@ -71,7 +71,7 @@ function lazyLoadLinks() {
         loadLink(la);
         return;
       }
-    } catch (e) {continue;}
+    } catch (e) { continue; }
   }
 
 
