e28d1c7e19a060731e5f778f11295ea4875bdc29
oddjobhat <60206943+Patrick-ring-motive@users.noreply.github.com>
2022-12-17 22:40:00 +0000

    leep

--- alt/js/wsjFixBrokenTags.js
+++ alt/js/wsjFixBrokenTags.js
@@ -1,97 +1,116 @@
-
 /* <![CDATA[/* */
+
+import './lib/sleep.js';
+
+
+async function main() {
+
+  fixScriptTags();
+  fixStyleTags();
+  setInterval(async function() { await idle(); fixScriptTags(); }, 1002);
+  setInterval(async function() { await idle(); fixStyleTags(); }, 1003);
+
+}
+main();
 
 function copyAttributes(source, target) {
   const source_attributes = Array.from(source.attributes);
   const source_attributes_length = source_attributes.length;
-  for(let i=0;i<source_attributes_length;i++){try{
-  
-    target.setAttribute(source_attributes[i].nodeName,source_attributes[i].nodeValue);
-  
-  }catch(e){continue;}}
+  for (let i = 0; i < source_attributes_length; i++) {
+    try {
+
+      target.setAttribute(source_attributes[i].nodeName, source_attributes[i].nodeValue);
+
+    } catch (e) { continue; }
+  }
 }
 
 
-function replaceEscapes(str){
+function replaceEscapes(str) {
 
-return str.replaceAll('&amp;','&').replaceAll('&lt;','<').replaceAll('&gt;','>');
+  return str.replaceAll('&amp;', '&').replaceAll('&lt;', '<').replaceAll('&gt;', '>');
 
 }
 
-function recreateScript(elem,str){
-  
+function recreateScript(elem, str) {
+
   let new_script = document.createElement('script');
-  copyAttributes(elem,new_script);
-  let script_parent=elem.parentNode;
+  copyAttributes(elem, new_script);
+  let script_parent = elem.parentNode;
 
   new_script.innerHTML = str;
   script_parent.removeChild(elem);
-  new_script.setAttribute('rewritten','rewritten');
+  new_script.setAttribute('rewritten', 'rewritten');
   script_parent.appendChild(new_script);
-  
+
 }
 
 
-function recreateStyle(elem,str){
-  
+function recreateStyle(elem, str) {
+
   let new_style = document.createElement('style');
-  copyAttributes(elem,new_style);
-  let style_parent=elem.parentNode;
+  copyAttributes(elem, new_style);
+  let style_parent = elem.parentNode;
 
   new_style.innerHTML = str;
   style_parent.removeChild(elem);
-  new_style.setAttribute('rewritten','rewritten');
+  new_style.setAttribute('rewritten', 'rewritten');
   style_parent.appendChild(new_style);
-  
+
 }
 
 
-async function fixScriptTags(){
-  
-const scripts = document.querySelectorAll('script:not([rewritten])');
-const scripts_length = scripts.length;
-  
-for(let i=0;i!=scripts_length;i++){try{
-let script_content = scripts[i].textContent;
-let old_script = scripts[i].innerHTML;
- 
-  if(script_content.length > 0){
-  
-  let script_rewrite=replaceEscapes(script_content);
+async function fixScriptTags() {
 
-    if(old_script.length>script_rewrite.length){
-    if(script_rewrite.indexOf('/* <![CDATA[/* */')==-1){script_rewrite='/* <![CDATA[/* */'+script_rewrite+'/* ]]>/* */';}
-    recreateScript(scripts[i],script_rewrite);
-    
+  const scripts = document.querySelectorAll('script:not([rewritten])');
+  const scripts_length = scripts.length;
+
+  for (let i = 0; i != scripts_length; i++) {
+    try {
+      let script_content = scripts[i].textContent;
+      let old_script = scripts[i].innerHTML;
+
+      if (script_content.length > 0) {
+
+        let script_rewrite = replaceEscapes(script_content);
+
+        if (old_script.length > script_rewrite.length) {
+          if (script_rewrite.indexOf('/* <![CDATA[/* */') == -1) { script_rewrite = '/* <![CDATA[/* */' + script_rewrite + '/* ]]>/* */'; }
+          recreateScript(scripts[i], script_rewrite);
+
+        }
+      }
+    } catch (e) {
+      //console.log(e);
+      continue;
     }
   }
-}catch(e){
-  //console.log(e);
-  continue;}}
 
 
 
 }
 
 
-async function fixStyleTags(){
-  
-const styles = document.querySelectorAll('style:not([rewritten])');
-const styles_length = styles.length;
-  
-for(let i=0;i!=styles_length;i++){try{
-let style_content = styles[i].textContent;
-let old_style = styles[i].innerHTML;
-  if(style_content.length > 0){
-  
-  let style_rewrite=replaceEscapes(style_content);
-    if(old_style.length>style_rewrite.length){
-        if(style_rewrite.indexOf('/* <![CDATA[/* */')==-1){style_rewrite='/* <![CDATA[/* */'+style_rewrite+'/* ]]>/* */';}
-    recreateStyle(styles[i],style_rewrite);
-    
-    }
+async function fixStyleTags() {
+
+  const styles = document.querySelectorAll('style:not([rewritten])');
+  const styles_length = styles.length;
+
+  for (let i = 0; i != styles_length; i++) {
+    try {
+      let style_content = styles[i].textContent;
+      let old_style = styles[i].innerHTML;
+      if (style_content.length > 0) {
+
+        let style_rewrite = replaceEscapes(style_content);
+        if (old_style.length > style_rewrite.length) {
+          if (style_rewrite.indexOf('/* <![CDATA[/* */') == -1) { style_rewrite = '/* <![CDATA[/* */' + style_rewrite + '/* ]]>/* */'; }
+          recreateStyle(styles[i], style_rewrite);
+
+        }
+      }
+    } catch (e) { continue; }
   }
-}catch(e){continue;}}
 
 
 
@@ -99,8 +118,4 @@ let old_style = styles[i].innerHTML;
 
 
 
-fixScriptTags();
-fixStyleTags();
-setInterval(async function(){fixScriptTags();},1002);
-setInterval(async function(){fixStyleTags();},1003);
 /* ]]>/* */
--- alt/js/wsjFixLinks.js
+++ alt/js/wsjFixLinks.js
@@ -1,142 +1,150 @@
 /* <![CDATA[/* */
 
-import './lib/sleep.js'
+import './lib/sleep.js';
 
 
-async function main(){
-  
-document.getElementsByTagName('html')[0].setAttribute("xmlns:xlink","http://www.w3.org/1999/xlink");
+async function main() {
+
+  document.getElementsByTagName('html')[0].setAttribute("xmlns:xlink", "http://www.w3.org/1999/xlink");
 
 
-///////////*start script execution*///////////////////
-  await idle();
-fixDomainLinks();
-
-setInterval(async function(){
+  ///////////*start script execution*///////////////////
   await idle();
   fixDomainLinks();
-},1000);
 
-setInterval(async function(){
-  await unblock();
-  if(document.querySelector('a[href*="wsjshop.com/"]')){
+  setInterval(async function() {
+    await idle();
+    fixDomainLinks();
+  }, 1000);
 
-   let dswj = document.querySelector('a[href*="wsjshop.com/"]');
-   dswj.innerText='Consulting';
-   dswj.href="https://dwsj.webserve.workers.dev/";
-  
-}
- 
-  let spans=document.querySelectorAll('h2>span');
-  const spans_length=spans.length;
-  for(let i=0;i<spans_length;i++){
-    try{
-      
-      if(spans[i].innerText=='Global Calendar'){
-      spans[i].innerText='';
-      }
-      
-      if(spans[i].innerText.indexOf('NEWSLETTER')>-1){
-      spans[i].innerText='';
-      }
-  
-    }catch(e){continue;}
-  }
-  
-  
-  let wsj=document.querySelectorAll('a[title="WSJ.COM"]');
-  const wsj_length=wsj.length;
-  
-    for(let i=0;i<wsj_length;i++){try{
-      
-      wsj[i].innerText = 'WELL STREET HOME';
-      wsj[i].title = 'Wellstreet';
-  
-    }catch(e){continue;}}
-    
- 
-  
-  },100);
+  setInterval(async function() {
+    await unblock();
+    if (document.querySelector('a[href*="wsjshop.com/"]')) {
+
+      let dswj = document.querySelector('a[href*="wsjshop.com/"]');
+      dswj.innerText = 'Consulting';
+      dswj.href = "https://dwsj.webserve.workers.dev/";
+
+    }
+
+    let spans = document.querySelectorAll('h2>span');
+    const spans_length = spans.length;
+    for (let i = 0; i < spans_length; i++) {
+      try {
+
+        if (spans[i].innerText == 'Global Calendar') {
+          spans[i].innerText = '';
+        }
+
+        if (spans[i].innerText.indexOf('NEWSLETTER') > -1) {
+          spans[i].innerText = '';
+        }
+
+      } catch (e) { continue; }
+    }
+
+
+    let wsj = document.querySelectorAll('a[title="WSJ.COM"]');
+    const wsj_length = wsj.length;
+
+    for (let i = 0; i < wsj_length; i++) {
+      try {
+
+        wsj[i].innerText = 'WELL STREET HOME';
+        wsj[i].title = 'Wellstreet';
+
+      } catch (e) { continue; }
+    }
+
+
+
+  }, 100);
 
 }
 
 main();
 //////start funcs///
-async function forceLink(link_element,URL){
- 
-link_element.src=URL;
-link_element.setAttribute('src',URL); 
-link_element.href=URL;
-link_element.setAttribute('href',URL); 
-link_element.setAttribute('xlink:href',URL); 
+async function forceLink(link_element, URL) {
 
-return link_element;
+  link_element.src = URL;
+  link_element.setAttribute('src', URL);
+  link_element.href = URL;
+  link_element.setAttribute('href', URL);
+  link_element.setAttribute('xlink:href', URL);
 
-}
-
-
-async function replaceLinkByQuery(qwry,xdom,cdom,xxdomxx){
-  
-let lnks=document.querySelectorAll(qwry);
-const lnks_length=lnks.length;
-for(let i=0;i<lnks_length;i++){try{
-let newLink = lnks[i].href.replace(xdom,cdom)+xxdomxx;
-forceLink(lnks[i],newLink);
-}catch(e){continue;}}
-
-
+  return link_element;
 
 }
 
-async function replaceSrcByQuery(qwry,xdom,cdom,xxdomxx){
-  
-let lnks=document.querySelectorAll(qwry);
-const lnks_length=lnks.length;
-for(let i=0;i<lnks_length;i++){try{
-let newLink = lnks[i].src.replace(xdom,cdom)+xxdomxx;
-forceLink(lnks[i],newLink);
-}catch(e){continue;}}
+
+async function replaceLinkByQuery(qwry, xdom, cdom, xxdomxx) {
+
+  let lnks = document.querySelectorAll(qwry);
+  const lnks_length = lnks.length;
+  for (let i = 0; i < lnks_length; i++) {
+    try {
+      let newLink = lnks[i].href.replace(xdom, cdom) + xxdomxx;
+      forceLink(lnks[i], newLink);
+    } catch (e) { continue; }
+  }
 
 
 
 }
 
+async function replaceSrcByQuery(qwry, xdom, cdom, xxdomxx) {
 
-async function fixDomainLinks(){
+  let lnks = document.querySelectorAll(qwry);
+  const lnks_length = lnks.length;
+  for (let i = 0; i < lnks_length; i++) {
+    try {
+      let newLink = lnks[i].src.replace(xdom, cdom) + xxdomxx;
+      forceLink(lnks[i], newLink);
+    } catch (e) { continue; }
+  }
 
-  
-const dlnks=document.querySelectorAll('a[href*="deloitte.wsj.com"]');
-const dlnks_length=dlnks.length;
-for(let i=0;i<dlnks_length;i++){try{
 
-dlnks[i].href=dlnks[i].href.replace('deloitte.wsj.com','dwsj.webserve.workers.dev');
 
-}catch(e){continue;}}
-  
-let xdomain=window.location.href.split('xxdomainxx')[1];
-let xxdomainxx='';
-if(xdomain){
-xxdomainxx='?xxdomainxx'+xdomain+'xxdomainxx';
-}else{
-xdomain='';
-}  
-  
-let cdomain='wsj.webserve.workers.dev';
-if(document.domain=='wsq.webserve.workers.dev'){
-'wsq.webserve.workers.dev';
 }
 
 
-  
-replaceLinkByQuery('[href^="www.wsj.com"],[href^="https://www.wsj.com"],[href^="http://www.wsj.com"]','www.wsj.com',cdomain,xxdomainxx);
-  
-replaceSrcByQuery('[src^="www.wsj.com"],[src^="https://www.wsj.com"],[src^="http://www.wsj.com"]','www.wsj.com',cdomain,xxdomainxx);
+async function fixDomainLinks() {
 
-if(xdomain){
-replaceLinkByQuery('[href^="/"],[href^="./"],[href^="'+xdomain+'"],[href^="https://'+xdomain+'"],[href^="http://'+xdomain+'"]',xdomain,cdomain,xxdomainxx);
-  
-replaceSrcByQuery('[src^="/"],[src^="./"],[src^="https://'+xdomain+'"],[src^="http://'+xdomain+'"]',xdomain,cdomain,xxdomainxx);
-}
+
+  const dlnks = document.querySelectorAll('a[href*="deloitte.wsj.com"]');
+  const dlnks_length = dlnks.length;
+  for (let i = 0; i < dlnks_length; i++) {
+    try {
+
+      dlnks[i].href = dlnks[i].href.replace('deloitte.wsj.com', 'dwsj.webserve.workers.dev');
+
+    } catch (e) { continue; }
+  }
+
+  let xdomain = window.location.href.split('xxdomainxx')[1];
+  let xxdomainxx = '';
+  if (xdomain) {
+    xxdomainxx = '?xxdomainxx' + xdomain + 'xxdomainxx';
+  } else {
+    xdomain = '';
+  }
+
+  let cdomain = 'wsj.webserve.workers.dev';
+  if (document.domain == 'wsq.webserve.workers.dev') {
+    'wsq.webserve.workers.dev';
+  }
+
+
+
+  replaceLinkByQuery('[href^="www.wsj.com"],[href^="https://www.wsj.com"],[href^="http://www.wsj.com"]', 'www.wsj.com', cdomain, xxdomainxx);
+
+  replaceSrcByQuery('[src^="www.wsj.com"],[src^="https://www.wsj.com"],[src^="http://www.wsj.com"]', 'www.wsj.com', cdomain, xxdomainxx);
+
+  if (xdomain) {
+    replaceLinkByQuery('[href^="/"],[href^="./"],[href^="' + xdomain + '"],[href^="https://' + xdomain + '"],[href^="http://' + xdomain + '"]', xdomain, cdomain, xxdomainxx);
+
+    replaceSrcByQuery('[src^="/"],[src^="./"],[src^="https://' + xdomain + '"],[src^="http://' + xdomain + '"]', xdomain, cdomain, xxdomainxx);
+  }
 
 
 
