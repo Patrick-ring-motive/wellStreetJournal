d8e377241f6eb62127257364ace6155822ee3f5b
oddjobhat <60206943+Patrick-ring-motive@users.noreply.github.com>
2022-12-31 02:43:55 +0000

    Sleep pack

--- alt/js/lib/sleep.js
+++ alt/js/lib/sleep.js
@@ -7,14 +7,26 @@ wandow.sleep = function(ms) {
   });
 }
 
-
-
-wandow.unblock = function() {
+wandow.block = function() {
   return new Promise((resolve) => {
-    requestIdleCallback(resolve, { timeout: 100 });
-    requestAnimationFrame(resolve);
+    queueMicroTask(resolve);
   });
 }
+
+wandow.doWork = function() {
+  return new Promise((resolve) => {
+    setTimeout(resolve,0);
+  });
+}
+
+wandow.unblock = function() {
+  return Promise.race([
+    new Promise((resolve) => requestIdleCallback(resolve, { timeout: 100 })),
+    new Promise((resolve) => requestAnimationFrame(resolve))
+  ]);
+}
+
+
 
 
 wandow.idleCheck = async function(resolve) {
@@ -30,3 +42,27 @@ wandow.idle = function() {
     idleCheck(resolve);
   });
 }
+
+wandow.delayWork = function() {
+  return 
+    Promise.all([requestIdleCallback(),
+    requestAnimationFrame()]);
+  
+}
+
+wandow.defer = async function() {
+
+  while (document.readyState !== "complete") {
+    await unblock();
+  }
+  return delayWork();
+}
+
+
+
+/*
+  The sleep function uses the setTimeout function to delay the execution of a function for a specified number of milliseconds. The unblock function uses the requestIdleCallback and requestAnimationFrame functions to schedule a callback to be executed during a period of idle time or at the next animation frame. The idleCheck function uses the unblock function to pause execution until the page has finished loading, and the idle function combines these functions to create a promise that resolves when the page has finished loading.
+
+These functions can be useful for scheduling tasks to be executed during idle time or after the page has finished loading, in order to improve the performance and responsiveness of your applications.
+
+*/
