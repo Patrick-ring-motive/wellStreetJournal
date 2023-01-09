86f512a4325186f767c24fb506e646fca8a17684
oddjobhat <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-01-09 17:57:50 +0000

    moar defer

--- alt/js/wsjImports.js
+++ alt/js/wsjImports.js
@@ -1,32 +1,33 @@
 /* <![CDATA[/* */
 
-setTimeout(async function(){
-  
-const URLs=['/alt/js/sw/swRegister.js','/alt/js/wsjGoogleAnalytics.js','/alt/js/dwsjGoogleAnalytics.js','/alt/js/wsjFixLinks.js','/alt/js/wsjSeo.js','/alt/js/wsjFixBrokenTags.js','/alt/js/lib/lazyLoadLinks.js'];
-const URLs_length=URLs.length;
+setTimeout(async function() {
 
-for(let i=0;i<URLs_length;i++){try{
+  const URLs = ['/alt/js/sw/swRegister.js', '/alt/js/wsjGoogleAnalytics.js', '/alt/js/dwsjGoogleAnalytics.js', '/alt/js/wsjFixLinks.js', '/alt/js/wsjSeo.js', '/alt/js/wsjFixBrokenTags.js', '/alt/js/lib/lazyLoadLinks.js'];
+  const URLs_length = URLs.length;
 
-wsjImportScript(URLs[i]);
+  for (let i = 0; i < URLs_length; i++) {
+    try {
 
-}catch(e){continue;}}
+      wsjImportScript(URLs[i]);
+
+    } catch (e) { continue; }
+  }
 
 });
 
-async function wsjImportScript(URL)
-{
-URL=URL+'?wellStreetJournalAlt';
-let script_element = document.createElement('script');
-script_element.async='async';
-script_element.setAttribute('async','async');
-script_element.type='text/javascript';
-script_element.setAttribute('type','text/javascript');  
-script_element.src=URL;
-script_element.setAttribute('src',URL); 
-script_element.href=URL;
-script_element.setAttribute('href',URL); 
-script_element.setAttribute('xlink:href',URL); 
-return document.body.appendChild(script_element);
+async function wsjImportScript(URL) {
+  URL = URL + '?wellStreetJournalAlt';
+  let script_element = document.createElement('script');
+  script_element.async = 'defer';
+  script_element.setAttribute('defer', 'defer');
+  script_element.type = 'text/javascript';
+  script_element.setAttribute('type', 'text/javascript');
+  script_element.src = URL;
+  script_element.setAttribute('src', URL);
+  script_element.href = URL;
+  script_element.setAttribute('href', URL);
+  script_element.setAttribute('xlink:href', URL);
+  return document.body.appendChild(script_element);
 }
 
 
