17e73f6841cc7053b42563cef2ca44f094c2c043
oddjobhat <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-01-24 18:02:58 +0000

    Suspend Service Worker on Safari

--- alt/js/sw/swRegister.js
+++ alt/js/sw/swRegister.js
@@ -1,7 +1,10 @@
 
-
-
+void async function RegisterServiceWorker(){
+if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
+return 0;
+}
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js?wellStreetJournalAlt');
   }
 
+}();
