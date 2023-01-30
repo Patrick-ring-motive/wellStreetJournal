e61fcea7655c1efba8a48db66f8f2bcdbd03938a
oddjobhat <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-01-30 02:50:53 +0000

    Slow

--- alt/js/src/altIndex.js
+++ alt/js/src/altIndex.js
@@ -5,5 +5,5 @@ import '../wsjGoogleAnalytics.js';
 import '../dwsjGoogleAnalytics.js';
 //import '../wsjFixLinks.js';
 import '../wsjSeo.js';
-import '../wsjFixBrokenTags.js';
-import '../lib/lazyLoadLinks.js';
+//import '../wsjFixBrokenTags.js';
+//import '../lib/lazyLoadLinks.js';
